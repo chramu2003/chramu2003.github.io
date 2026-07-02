7b3f40ff5736d73aa3f6ce99647eee601e574ebd
chramu2003 <36374029+chramu2003@users.noreply.github.com>
2026-07-02 20:22:08 +0200

    Add typing effect and theme toggle functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+new Typed('#typed',{
+strings:[
+'Senior Java Engineer',
+'Enterprise Architect',
+'SAP GRC Consultant',
+'AWS Cloud Engineer',
+'AI Engineer'
+],
+typeSpeed:60,
+backSpeed:40,
+loop:true
+});
+document.getElementById('themeBtn').onclick=()=>{
+document.body.classList.toggle('light');
+}
